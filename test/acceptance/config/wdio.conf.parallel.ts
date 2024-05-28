acb366f41b0713be6b5b359441ebf16327f0153b
Giuseppe Salvatore <giuseppe.salvatore@gmail.com>
2024-05-28 17:02:01 +0100

    [PASS] Tested the project with 8.38.1

---
+++ test/acceptance/config/wdio.conf.parallel.ts
@@ -0,0 +1,18 @@
+import type { Options } from "@wdio/types";
+import { globalConfig } from "./wdio.conf.base";
+
+globalConfig.capabilities = [{
+    maxInstances: 5,
+    browserName: "firefox",
+    browserVersion: "latest"
+}, {
+    maxInstances: 5,
+    browserName: "MicrosoftEdge",
+    browserVersion: "stable"
+}, {
+    maxInstances: 5,
+    browserName: "chrome",
+    browserVersion: "stable"
+},];
+
+export const config: Options.Testrunner = globalConfig;
