c7595a44a89d4c45dca8823ab07abba6a00d88d4
Giuseppe Salvatore <giuseppe.salvatore@gmail.com>
2024-06-30 22:07:07 +0100

    Fixed problem with vulnerable ws package
    Forxed resolution to fetch version ^8.17.1

--- scripts/packagejson-fixup.py
+++ scripts/packagejson-fixup.py
@@ -7,20 +7,35 @@ with open(targetFile, "r") as jsonFile:
 
 if "type" in data:
     del data["type"]
-data["resolutions"] = {"mockery": "2.1.0"}
+data["resolutions"] = {
+    "mockery": "2.1.0", 
+    "ws": "^8.17.1"
+}
 data["name"] = "webdriverio-autoinit"
 
-data["scripts"]["ci"] = \
-    "yarn install --frozen-lockfile"
-data["scripts"]["format"] = \
-    "prettier --write \"./**/*.{js,jsx,ts,tsx,json}\""
-data["scripts"]["test:acceptance:desktop"] = "wdio run ./test/acceptance/config/wdio.conf.chrome.ts"
-data["scripts"]["test:acceptance:desktop:edge"] = "wdio run ./test/acceptance/config/wdio.conf.edge.ts"
-data["scripts"]["test:acceptance:desktop:firefox"] = "wdio run ./test/acceptance/config/wdio.conf.firefox.ts"
-data["scripts"]["test:acceptance:desktop:parallel"] = "wdio run ./test/acceptance/config/wdio.conf.parallel.ts"
-data["scripts"]["test:acceptance:desktop"] = "wdio run ./test/acceptance/config/wdio.conf.chrome.ts"
-data["scripts"]["test:acceptance:mobile"]  = "TARGET_DEVICE_VIEW=iphone-12 wdio run ./test/acceptance/config/wdio.conf.chrome.ts"
-data["scripts"]["test:acceptance:desktop:ci"] = "RUNTIME_ENV=CI wdio run ./test/acceptance/config/wdio.conf.chrome.ts"
+data["scripts"]["ci"] = "yarn install --frozen-lockfile"
+data["scripts"]["format"] = 'prettier --write "./**/*.{js,jsx,ts,tsx,json}"'
+data["scripts"][
+    "test:acceptance:desktop"
+] = "wdio run ./test/acceptance/config/wdio.conf.chrome.ts"
+data["scripts"][
+    "test:acceptance:desktop:edge"
+] = "wdio run ./test/acceptance/config/wdio.conf.edge.ts"
+data["scripts"][
+    "test:acceptance:desktop:firefox"
+] = "wdio run ./test/acceptance/config/wdio.conf.firefox.ts"
+data["scripts"][
+    "test:acceptance:desktop:parallel"
+] = "wdio run ./test/acceptance/config/wdio.conf.parallel.ts"
+data["scripts"][
+    "test:acceptance:desktop"
+] = "wdio run ./test/acceptance/config/wdio.conf.chrome.ts"
+data["scripts"][
+    "test:acceptance:mobile"
+] = "TARGET_DEVICE_VIEW=iphone-12 wdio run ./test/acceptance/config/wdio.conf.chrome.ts"
+data["scripts"][
+    "test:acceptance:desktop:ci"
+] = "RUNTIME_ENV=CI wdio run ./test/acceptance/config/wdio.conf.chrome.ts"
 
 
 with open(targetFile, "w") as jsonFile:
