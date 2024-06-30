3e4751a2bf0fdfe408306cb55264dfb52357659c
Giuseppe Salvatore <giuseppe.salvatore@gmail.com>
2024-06-30 21:58:53 +0100

    [PASS] Tested the project with 8.39.0

---
+++ init.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+rm -rf webdriverio-autoinit
+git clone git@github.com:giuseppe-salvatore/webdriverio-autoinit.git
+cd webdriverio-autoinit
+
+# Need to setup a bit of environment
+python3 -m pip install --user virtualenv
+python3 -m venv ./scripts/.pyvenv/
+source ./scripts/.pyvenv/bin/activate
+source ~/.nvm/nvm.sh
+
+echo "Building the projct using node 18"
+nvm use 18
+npm install -g yarn
+make
+
+echo "Building the project using node 20"
+nvm use 20
+npm install -g yarn
+make 
+
+VERSION=$(make version)
+echo "Currently tested version ${VERSION}"
+git branch -vva|grep ${VERSION}
+
+if [ $? == 1 ];
+then
+	git checkout -b test/${VERSION}-pass
+	git add .
+	git commit -m "[PASS] Tested the project with ${VERSION}" 
+else
+	echo "It looks like you already have a remote branch with this version, so skipping the local commit"
+fi
+
+cd ..
+
+if [ ! -d "${VERSION}" ];
+then
+	mv webdriverio-autoinit ${VERSION}
+else
+	mv ${VERSION} ${VERSION}-obsolete
+	mv webdriverio-autoinit ${VERSION}
+fi
+
+if [ -L "latest" ];
+then
+	rm latest
+fi
+ln -s ${VERSION} latest
+
--- test/acceptance/config/wdio.conf.chrome.ts
+++ test/acceptance/config/wdio.conf.chrome.ts
@@ -1,6 +1,7 @@
 import type { Options } from "@wdio/types";
 import { globalConfig } from "./wdio.conf.base";
 
+<<<<<<< HEAD
 const RUNTIME_ENV: string = process.env.RUNTIME_ENV || "CI";
 
 const chromeStable: Record<string, any> = {
@@ -18,5 +19,12 @@ if (RUNTIME_ENV == "CI") {
 }
 
 globalConfig.capabilities = [chromeStable];
+=======
+globalConfig.capabilities = [{
+    maxInstances: 5,
+    browserName: "chrome",
+    browserVersion: "stable"
+}];
+>>>>>>> b97517c ([PASS] Tested the project with 8.39.0)
 
 export const config: Options.Testrunner = globalConfig;
