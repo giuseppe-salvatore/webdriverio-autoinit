7df8443ccb50208bba9f143fd89c3c75270fe08a
Giuseppe Salvatore <giuseppe.salvatore@gmail.com>
2024-08-19 13:57:46 +0100

    Fixed steps definitions for new functions

--- features/step-definitions/steps.ts
+++ features/step-definitions/steps.ts
@@ -18,6 +18,6 @@ When(/^I login with (\w+) and (.+)$/, async (username, password) => {
 
 Then(/^I should see a flash message saying (.*)$/, async (message) => {
     await expect(SecurePage.flashAlert).toBeExisting();
-    await expect(SecurePage.flashAlert).toHaveTextContaining(message);
+    await expect(await SecurePage.flashAlert.getText()).toContain(message);
 });
 
